e75db4c6d787c13231e495d34ead73366f12a1c9
Rob Hailman <robhailman@gmail.com>
2018-12-01 13:42:51 -0500

    Begin writing player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum AnimState
+{
+    IDLE,
+    WALKING,
+}
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Sprite Properties")]
+    [SerializeField] private AnimState animState;
+    [SerializeField] private Sprite[] walkSprites;
+    [SerializeField] private Sprite idleSprite;
+    [SerializeField] private float walkSpriteTime;
+
+    private int currentAnimSprite;
+    private float nextAnimSpriteTime;
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoAnimation();
+    }
+
+    void DoAnimation()
+    {
+        switch (animState)
+        {
+            case AnimState.IDLE:
+                spriteRenderer.sprite = idleSprite;
+                nextAnimSpriteTime = Time.time;
+                break;
+            case AnimState.WALKING:
+                if (Time.time > nextAnimSpriteTime)
+                {
+                    currentAnimSprite++;
+                    currentAnimSprite %= walkSprites.Length;
+                    spriteRenderer.sprite = walkSprites[currentAnimSprite];
+                    nextAnimSpriteTime += walkSpriteTime;
+                }
+                break;
+        }
+    }
+}
