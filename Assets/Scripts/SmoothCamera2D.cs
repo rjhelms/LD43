8450f988d2663912d3d01b25cb0e624e3057296e
Rob Hailman <robhailman@gmail.com>
2018-12-01 23:09:11 -0500

    Add offset & initial update to SmoothCamera2D

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -5,8 +5,19 @@ public class SmoothCamera2D : MonoBehaviour
 {
 
     public float dampTime = 0.15f;
+    public Vector3 ViewOffset;
     private Vector3 velocity = Vector3.zero;
     public Transform target;
+
+    private void Start()
+    {
+        if (target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            transform.position = transform.position + delta + ViewOffset;
+        }
+    }
 
     // Update is called once per frame
     void LateUpdate()
@@ -15,7 +26,7 @@ public class SmoothCamera2D : MonoBehaviour
         {
             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
             Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = transform.position + delta;
+            Vector3 destination = transform.position + delta + ViewOffset;
             Vector3 newPosition = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
             transform.position = GetComponent<UnityEngine.U2D.PixelPerfectCamera>().RoundToPixel(newPosition);
         }
